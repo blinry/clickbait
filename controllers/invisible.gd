6b420306ba7444ec61696082a17406073ea8bc89
bleeptrack <mail@bleeptrack.de>
2020-07-11 19:58:30 +0200

    invisible fix

--- controllers/invisible.gd
+++ controllers/invisible.gd
@@ -1,7 +1,10 @@
 extends Node
 
+func _ready():
+    $Cursor/cursor.visible = false
+
 func _input(event):
     if event is InputEventMouseMotion:
         var delta = event.get_relative()
         $Cursor.position += delta
-        $Cursor/cursor.visible = false
+        
