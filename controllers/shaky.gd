6c02c055c9d77e7672fcdd385004fa9608ac3139
bleeptrack <mail@bleeptrack.de>
2020-07-11 13:06:35 +0200

    Shaky Mouse
    is shaky

--- controllers/shaky.gd
+++ controllers/shaky.gd
@@ -2,9 +2,17 @@ extends Node
 
 signal click(pos)
 
+var shake = 25
+
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		emit_signal("click", event.position)
 	if event is InputEventMouseMotion:
 		var delta = event.get_relative()
 		$Cursor.position += delta
+		$Cursor.position.x += rand_range(-delta.length(), delta.length())
+		$Cursor.position.y += rand_range(-delta.length(), delta.length())
+		
+func _process(delta):
+	$Cursor.position.x += rand_range(-shake, shake)
+	$Cursor.position.y += rand_range(-shake, shake)
