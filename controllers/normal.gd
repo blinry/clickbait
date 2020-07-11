f6983b5bc271bea0bdb255d62e65ac7f10455e96
bleeptrack <mail@bleeptrack.de>
2020-07-11 12:53:32 +0200

    Auto stash before merge of "master" and "origin/master"

--- controllers/normal.gd
+++ controllers/normal.gd
@@ -8,3 +8,15 @@ func _input(event):
     if event is InputEventMouseMotion:
         var delta = event.get_relative()
         $Cursor.position += delta
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	var m = get_viewport().get_mouse_position()
+	$Cursor.position = m
