26571b7980b5dc24109c04a24de705ab7679fb94
Sebastian Morr <sebastian@morr.cc>
2020-07-11 14:30:58 +0200

    Show current controller in label

--- main.gd
+++ main.gd
@@ -1,29 +1,19 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var currentController = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    changeController(0)
 
 func _input(event):
-    
-    
     if event.is_action_pressed("next_controller"):
-        $Controllers.get_child(currentController).hide()
-        currentController = (currentController + 1) % $Controllers.get_child_count()
-        $Controllers.get_child(currentController).show()
+        changeController(1)
         
     if event.is_action_pressed("prev_controller"):
-        $Controllers.get_child(currentController).hide()
-        currentController = (currentController - 1) % $Controllers.get_child_count()
-        $Controllers.get_child(currentController).show()
-        
-        
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+        changeController(-1)
+       
+func changeController(diff):
+    $Controllers.get_child(currentController).hide()
+    currentController = (currentController + diff + $Controllers.get_child_count()) % $Controllers.get_child_count()
+    $Controllers.get_child(currentController).show()
+    $ControllerLabel.text = $Controllers.get_child(currentController).name
