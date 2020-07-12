e000d4a6888a2b8833c3759047df012e2c781d33
Sebastian Morr <sebastian@morr.cc>
2020-07-12 10:41:50 +0200

    Rocket emits particles

--- controllers/rocket.gd
+++ controllers/rocket.gd
@@ -9,3 +9,6 @@ func _process(delta):
     if Input.is_action_pressed("rightclick"):
         var direction = Vector2(0,-1).rotated(-PI/6 + $Cursor.rotation)
         $Cursor.velocity += direction/2
+        $Cursor/Particles.emitting = true
+    else:
+        $Cursor/Particles.emitting = false
